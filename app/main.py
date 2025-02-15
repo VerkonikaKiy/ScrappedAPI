1af37804dcada63ff3359bf3343a9ae062bce58d
VerkonikaKiy <verunya.kiyashko@mail>
2025-02-15 09:42:05 +0300

    models created

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI, Depends
 import os
 from typing import Optional
+from api.tasks import all_tasks
+
 
 app = FastAPI()
+app.include_router(all_tasks.router)
+
+
--- app/models/database.py
+++ app/models/database.py
@@ -1,9 +1,18 @@
 from sqlalchemy import create_engine
-
-from config import settings
+from sqlalchemy.orm import sessionmaker
+from models.config import settings
 
 engine = create_engine(
     url=settings.DATABASE_URL_psycopg,
     echo=False,
 )
 connection = engine.connect()
+
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
